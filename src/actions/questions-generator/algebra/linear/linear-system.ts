134d14ba2cf2c7164498bca2db1c0982de7b3a75
gbtsui <gabriel.tsui@protonmail.com>
2025-07-02 14:18:46 -0400

    linear system

---
+++ src/actions/questions-generator/algebra/linear/linear-system.ts
@@ -0,0 +1,107 @@
+"use server";
+
+import {DifficultyRanking, SystemResult} from "@/app/types";
+import RandomInt from "@/actions/reusable-utils/random-int";
+
+export default async function generateLinearSystemQuestion(difficulty: DifficultyRanking): Promise<SystemResult> {
+    if (difficulty === "easy") {
+        //format of:
+        // y = ax + b
+        // y = cx + d
+        const x = RandomInt(-5, 5, true);
+        const y = RandomInt(-5, 5);
+
+        let a, c;
+        do {
+            a = RandomInt(1, 10);
+            c = RandomInt(1, 10);
+        } while (a === c);
+
+        const b = y - a * x;
+        const d = y - c * x;
+
+        return {
+            equation_1: `y = ${a}x + ${b}`,
+            equation_2: `y = ${c}x + ${d}`,
+            solution: { x, y }
+        }
+    } else if (difficulty === "medium") {
+        //format of:
+        // ax + y = b
+        // cx + dy = e
+        //no negatives?
+
+        const x = RandomInt(-5, 5, true);
+        const y = RandomInt(-5, 5);
+
+        let a, c, d;
+        let det;
+        do {
+            a = RandomInt(1, 10);
+            c = RandomInt(1, 10);
+            d = RandomInt(1, 10);
+            det = c - d * a;
+        } while (det === 0);
+
+        const b = a * x + y;
+        const e = c * x + d * y;
+
+        return {
+            equation_1: `${a}x + y = ${b}`,
+            equation_2: `${c}x + ${d}y = ${e}`,
+            solution: { x, y }
+        }
+    } else {
+        //format of:
+        // ax + by = c
+        // dx + ey = f
+        //negatives allowed
+
+        /*
+        const a = RandomInt(-10, 10, true)
+        const b = RandomInt(-10, 10, true)
+        const c = RandomInt(-30, 30)
+        const d = RandomInt(-10, 10, true)
+        const e = RandomInt(-10, 10, true)
+        const f = RandomInt(-30, 30)
+         */
+
+        let det
+        let a,b,d,e
+        do {
+            a = RandomInt(-10, 10, true)
+            b = RandomInt(-10, 10, true)
+            d = RandomInt(-10, 10, true)
+            e = RandomInt(-10, 10, true)
+            det = (b * d) - (a * e)
+        } while (det === 0)
+
+        let f,c
+        c = RandomInt(-30, 30);
+        f = RandomInt(-30, 30);
+
+
+        // y = (c - ax)/b
+        // dx + e((c - ax) / b) = f
+        // dx + (ce - aex)/b = f
+        // bdx + ce - aex = fb
+        // bdx - aex = fb - ce
+        // (bd - ae)x = fb - ce
+        // x = (fb - ce) / (bd - ae)
+        let x = (f * b - c * e) / (b * d - a * e);
+        let y
+
+        if (b === 0) {
+            // use second equation
+            y = (f - d * x) / e;
+        } else {
+            y = (c - a * x) / b;
+        }
+
+        return {
+            equation_1: `${a}x + ${b}y = ${c}`,
+            equation_2: `${d}x + ${e}y = ${f}`,
+            solution: {x, y}
+        }
+    }
+}
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,3 +1,4 @@
 export type DifficultyRanking = "easy" | "medium" | "hard"
 
 export type EquationResult = {x: number, equation: string}
+export type SystemResult = {equation_1: string, equation_2: string, solution: {x: number, y: number} }
