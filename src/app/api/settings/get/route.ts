8d2f9f7013dd6a3a7a000001578f4887c7311070
Gabriel Tsui <148885745+gbtsui@users.noreply.github.com>
2025-07-03 19:44:40 -0400

    Dev (#1)
    * significant SettingsPage upgrades
    
    - moved data fetching to server side
    - optimized initial data filling
    - DatabaseUser type
    
    * removed unsafe api endpoint
    
    * Revert "removed unsafe api endpoint"
    
    This reverts commit b2b730a73a56065780dd970653013b8e1d8e0fb0.
    
    * secured unsafe api endpoint
    
    * slightly improved UX, removed unnecessary homepage signIn function

--- src/app/api/settings/get/route.ts
+++ src/app/api/settings/get/route.ts
@@ -1,26 +1,32 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import {prisma} from "@/lib/prisma";
+import {NextResponse} from "next/server";
+import CheckLoggedIn from "@/actions/auth/check-logged-in";
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const userId = searchParams.get("userId");
+    const {searchParams} = new URL(req.url);
+    const userId = searchParams.get("userId");
 
-  if (!userId) {
-    return NextResponse.json({ error: "Missing userId" }, { status: 400 });
-  }
-
-  try {
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      select: { bio: true, email: true, username: true, profile_picture_url: true },
-    });
-
-    if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    if (!userId) {
+        return NextResponse.json({error: "Missing userId"}, {status: 400});
     }
 
-    return NextResponse.json(user);
-  } catch (err) {
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
-  }
+    const user = await CheckLoggedIn()
+    if (!user || user.id !== userId) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {id: userId},
+            select: {bio: true, email: true, username: true, profile_picture_url: true},
+        });
+
+        if (!user) {
+            return NextResponse.json({error: "User not found"}, {status: 404});
+        }
+
+        return NextResponse.json(user);
+    } catch (err) {
+        return NextResponse.json({error: "Server error"}, {status: 500});
+    }
 }
