b01de095dc20bb695aad12222521285224d3f191
StephYSN <3011316681@qq.com>
2025-07-03 16:43:22 -0700

    add problem type menu. (TODO: change to selector-ish)

--- src/app/(pages)/play/page.tsx
+++ src/app/(pages)/play/page.tsx
@@ -3,11 +3,19 @@
 import { InlineMath, BlockMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
 import QuestionBox from "@/app/components/QuestionBox";
+import ProblemTypeMenu from '@/app/components/ProblemTypeMenu';
 
 export default function PlayPage() {
     return (
-        <div>
-            <QuestionBox question={{question: "9+10", solution: "21"}} />
+        <div className="h-dvh w-full flex bg-amber-100 text-stone-900">
+            <div className='w-60 p-2 bg-amber-200 rounded-lg'>
+                <ProblemTypeMenu />
+            </div>
+
+            {/* Main content */}
+            <div className="flex-1 flex items-center justify-center">
+                <QuestionBox question={{ question: "9+10", solution: "21" }} />
+            </div>
         </div>
     )
 }
