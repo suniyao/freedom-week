672f30219210197b5c44a5845ab2a986d5515b3e
StephYSN <3011316681@qq.com>
2025-07-04 16:42:16 -0700

    add the /

--- src/app/components/play/Play.tsx
+++ src/app/components/play/Play.tsx
@@ -16,7 +16,7 @@ export default function Play() {
         <div className="min-h-screen w-full flex bg-amber-100 text-stone-900">
             <div className='mt-5 ml-10'>
                 <div className="bg-amber-200 p-4 rounded-lg font-semibold w-30">
-                    <QuestionCounter totalQuestions={playSession.playMode === "infinity" ? "∞" : playSession.questions.length} currentQuestionIndex={playSession.attemptedQuestions.length - 1} questionStatuses={[]}>
+                    <QuestionCounter totalQuestions={playSession.playMode === "infinity" ? "∞" : playSession.questions.length} currentQuestionIndex={playSession.attemptedQuestions.length - 1} questionStatuses={[]}/>
                 </div>
             </div>
             <div className="flex-1 flex items-center justify-center">
@@ -27,6 +27,7 @@ export default function Play() {
                 <div className='bg-amber-200 p-4 rounded-lg font-semibold w-30'>
                     <Score score={score}/>
                 </div>
+            </div>
             </div>
     )
 }
