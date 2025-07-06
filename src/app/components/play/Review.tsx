f8bf72d9ed3131f415a6b3a074ecc621d3a6bf86
StephYSN <3011316681@qq.com>
2025-07-06 11:55:45 -0700

    better review ui

--- src/app/components/play/Review.tsx
+++ src/app/components/play/Review.tsx
@@ -1,25 +1,26 @@
 import SingleProblemReview from "./SingleProblemReview";
-import {usePlaySession} from "@/app/components/play/PlaySessionContext";
-import {QuestionAttemptData} from "@/app/types";
+import { usePlaySession } from "@/app/components/play/PlaySessionContext";
+import { QuestionAttemptData } from "@/app/types";
 
 export default function Review() {
-    const playSession = usePlaySession();
+  const playSession = usePlaySession();
 
-    return (
-        <div className="min-h-screen w-full bg-amber-50 text-stone-900 py-16 px-4 sm:px-8">
-            <div className="max-w-3xl mx-auto space-y-6">
-                <h1 className="text-3xl font-bold text-center mb-8">Review yuor'ue Answers</h1>
-                <div className={"flex flex-row"}>
-                    <div className={"flex flex-col w-1/2 p-3 m-2 bg-amber-300"}>
-                        {playSession.attemptedQuestions.map((question: QuestionAttemptData, index) => {
-                            return <SingleProblemReview key={index} index={index} attempt={question}/>
-                        })}
-                    </div>
-                    <div>
-                        put some stuff here
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="min-h-screen w-full bg-amber-50 text-stone-900 py-16 px-4 sm:px-8">
+      <div className="max-w-3xl mx-auto space-y-6">
+        <h1 className="text-3xl font-bold text-center mb-8">Review Your Answers</h1>
+        <div className="flex flex-col gap-6">
+          {playSession.attemptedQuestions.map(
+            (question: QuestionAttemptData, index) => (
+              <SingleProblemReview
+                key={index}
+                index={index}
+                attempt={question}
+              />
+            )
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
