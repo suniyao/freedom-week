9dfe3972345365527daac5bb28bd2c212d41f802
gbtsui <gabriel.tsui@protonmail.com>
2025-07-05 18:06:21 -0700

    functional binomial expansion marking

--- src/actions/questions-marker/algebra/expansion/mark-binomial-expansion.ts
+++ src/actions/questions-marker/algebra/expansion/mark-binomial-expansion.ts
@@ -3,7 +3,7 @@ import normalizeMathExpression from "@/actions/reusable-utils/normalize-math-exp
 
 
 export default function markBinomialExpansion(providedAnswer: Record<string, string|number>, solution: Record<string, string|number>): boolean {
-    const answer = providedAnswer.ans.toString();
+    const answer = `${providedAnswer.a.toString()}x^2 + ${providedAnswer.b.toString()}x + ${providedAnswer.c.toString()}`
     const actual_solution = solution.ans.toString();
     return expressionsAreEqual(normalizeMathExpression(answer), normalizeMathExpression(actual_solution));
 }
